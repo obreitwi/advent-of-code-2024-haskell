e8b40988bc99b62bf7c5b47ac1bfc7196a63689b
Oliver Breitwieser <oliver@breitwieser.eu>
2024-12-01 23:34:15 +0100

    feat: day01 part 1

--- day01/day01.hs
+++ day01/day01.hs
@@ -3,16 +3,28 @@
 
 module Main where
 
-import qualified Data.Text.IO as TIO
-import qualified Data.ByteString as BS
+import Data.Attoparsec.Text hiding (take)
 import Data.FileEmbed (embedFileRelative)
-
-input :: BS.ByteString
-input = $(embedFileRelative "./input")
+import Data.Text qualified as T
+import Data.Text.Encoding (decodeUtf8Lenient)
+import Rebase.Prelude hiding (left, right, takeWhile)
+import Prelude ()
 
 main :: IO ()
 main = do
-   TIO.putStrLn "Hello, Advent of Code!"
-   let numBytes = BS.length input
-   putStrLn $ "First input is " ++ show numBytes ++ " bytes long."
+  putStr "Part 1: "
+  either error (print . part1) $ parseOnly parseInput1 input
 
+input :: T.Text
+input = decodeUtf8Lenient $(embedFileRelative "./input")
+
+part1 :: ([Int], [Int]) -> Int
+part1 (left, right) = sum $ map abs $ zipWith (-) (sort left) (sort right)
+
+parseInput1 :: Parser ([Int], [Int])
+parseInput1 = do
+  pairs <- many1' $ parsePair <* endOfLine
+  return . unzip $ pairs
+  where
+    parsePair :: Parser (Int, Int)
+    parsePair = (,) <$> (decimal <* skipSpace) <*> decimal
