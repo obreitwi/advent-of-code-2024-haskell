6396d0ada990558f554481c303f2b898c9705a03
Oliver Breitwieser <oliver@breitwieser.eu>
2024-12-02 23:25:19 +0100

    feat(day02): part 1 + 2

--- day02/day02.hs
+++ day02/day02.hs
@@ -13,23 +13,32 @@ import Prelude ()
 main :: IO ()
 main = do
   putStr "Part 1: "
-  either error (print . part1) $ parseOnly parseInput1 input
+  either error (print . part1) $ parseOnly parseInput input
   putStr "Part 2: "
-  either error (print . part2) $ parseOnly parseInput1 input
+  either error (print . part2) $ parseOnly parseInput input
 
 input :: T.Text
 input = decodeUtf8Lenient $(embedFileRelative "./input")
 
-part1 :: ([Int], [Int]) -> Int
-part1 (left, right) = sum $ map abs $ zipWith (-) (sort left) (sort right)
+part1 :: [[Int]] -> Int
+part1 = length . filter (\l -> isValidAscending l || isValidDescending l)
 
-part2 :: ([Int], [Int]) -> Int
-part2 (left, right) = sum $ map (\l -> l * length (filter (l ==) right)) left
+part2 :: [[Int]] -> Int
+part2 = length . filter (\l -> drop1Valid isValidAscending l || drop1Valid isValidDescending l)
 
-parseInput1 :: Parser ([Int], [Int])
-parseInput1 = do
-  pairs <- many1' $ parsePair <* endOfLine
-  return . unzip $ pairs
-  where
-    parsePair :: Parser (Int, Int)
-    parsePair = (,) <$> (decimal <* skipSpace) <*> decimal
+isValidAscending :: [Int] -> Bool
+isValidAscending l = all (\e -> e > 0 && e < 4) $ zipWith (-) (drop 1 l) l
+
+isValidDescending :: [Int] -> Bool
+isValidDescending l = all (\e -> e < 0 && e > -4) $ zipWith (-) (drop 1 l) l
+
+dropNth :: [Int] -> Int -> [Int]
+dropNth [] _ = []
+dropNth (_ : ll) 1 = ll
+dropNth (l : ll) n = l : dropNth ll (n - 1)
+
+drop1Valid :: ([Int] -> Bool) -> [Int] -> Bool
+drop1Valid isValid l = any (isValid . dropNth l) [1 .. length l]
+
+parseInput :: Parser [[Int]]
+parseInput = (decimal `sepBy1'` char ' ') `sepBy1'` endOfLine
